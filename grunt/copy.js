d2d56fc19a7361b0ef54c77e2383508c1de2064e
William Belle <william.belle@gmail.com>
2016-12-03 22:16:07 +0100

    add firefox build

--- grunt/copy.js
+++ grunt/copy.js
@@ -1,21 +1,47 @@
-// Copy files
-module.exports = {
-  buildChrome: {
-    files: [{
-      expand: true,
-      cwd: 'src/',
-      src: ['js/**', 'css/**', 'images/**', '*.html', 'manifest.json',],
-      dest: 'dist/chrome/',
-      nonull: true,
-    },],
-  },
-  buildFirefox: {
-    files: [{
-      expand: true,
-      cwd: 'src/',
-      src: ['js/**', 'css/**', 'images/**', '*.html', 'manifest.firefox.json',],
-      dest: 'dist/firefox/',
-      nonull: true,
-    },],
-  },
+module.exports = function(grunt, options) {
+  'use strict';
+
+  return {
+    buildChrome: {
+      files: [{
+        expand: true,
+        cwd: 'src/',
+        src: ['js/**', 'css/**', 'images/**', '*.html', 'manifest.json'],
+        dest: 'dist/chrome/',
+        nonull: true,
+      },{
+        expand: true,
+        cwd: 'bower_components/jquery/dist/',
+        src: [ 'jquery.min.js' ],
+        dest: 'dist/chrome/js/',
+        nonull: true,
+      },],
+    },
+    buildFirefox: {
+      files: [{
+        expand: true,
+        cwd: 'src/',
+        src: ['js/**', 'css/**', 'images/**', '*.html',],
+        dest: 'dist/firefox/',
+        nonull: true,
+      },{
+        expand: true,
+        cwd: 'src/',
+        src: ['manifest.firefox.json',],
+        dest: 'dist/firefox/',
+        nonull: true,
+        rename: function(destBase, destPath) {
+          return destBase + destPath.replace(
+            'manifest.firefox.json', 'manifest.json'
+          );
+        },
+      },{
+        expand: true,
+        cwd: 'bower_components/jquery/dist/',
+        src: [ 'jquery.min.js' ],
+        dest: 'dist/firefox/js/',
+        nonull: true,
+      },],
+    },
+  };
 };

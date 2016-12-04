22a9d73461cbab7523faf8435ecbb2ce074a3366
William Belle <william.belle@gmail.com>
2016-12-04 17:27:08 +0100

    add content script

---
+++ src/js/contentscript.js
@@ -0,0 +1,9 @@
+'use strict';
+
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if (request === 'getHTML') {
+      sendResponse(document.all[0].outerHTML);
+    }
+  }
+);

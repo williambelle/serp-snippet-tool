a6c1eedf188b32bd2d3329b5e9180a25c1d750a2
William Belle <william.belle@gmail.com>
2016-12-05 19:42:56 +0100

    refactor popup.js

--- src/js/popup.js
+++ src/js/popup.js
@@ -23,43 +23,13 @@ function retrieveData() {
 }
 
 function processHtml(data,tab) {
-  var htmlObj = buildHtml(data);
+  var htmlObj = serpSnippetTool.utils.buildHtml(data);
   var serp = {
-    title: buildTitle(tab.title),
-    url: buildUrl(tab.url),
-    description: buildDescription(htmlObj),
+    title: serpSnippetTool.utils.buildTitle(tab.title),
+    url: serpSnippetTool.utils.buildUrl(tab.url),
+    description: serpSnippetTool.utils.buildDescription(htmlObj),
   };
   updatePopup(serp);
-}
-
-function buildHtml(htmlString) {
-  var htmlObj = $('<div></div>');
-  htmlObj.html(htmlString.match(/<head[^>]*>[\s\S]*<\/head>/gi));
-  return htmlObj;
-}
-
-function buildDescription(htmlObj) {
-  var description = htmlObj.find('meta[name=description]').attr('content') ||
-    htmlObj.find('meta[property=\'og\\:title\']').attr('content') ||
-    ' ';
-  if (description.length > 156) {
-    description = description.substring(0,156) + ' ...';
-  }
-  return description.trim();
-}
-
-function buildTitle(title) {
-  if (title.length > 60) {
-    title = title.substring(0,60) + ' ...';
-  }
-  return title.trim();
-}
-
-function buildUrl(url) {
-  if (url.length > 69) {
-    url = url.substring(0,69) + ' ...';
-  }
-  return url;
 }
 
 function updatePopup(serp) {

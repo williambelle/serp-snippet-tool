e8fe670da43de8bff5162e31433b3d61f1164787
William Belle <william.belle@gmail.com>
2016-12-05 20:01:07 +0100

    test url

--- test/test-utils.js
+++ test/test-utils.js
@@ -18,5 +18,20 @@
       var expLongTitle = serpSnippetTool.utils.buildTitle(longTitle);
       expLongTitle.should.equal(expectedLongTitle);
     });
+
+    it('should return a nice url', function() {
+      var shortUrl = 'https://github.com/williambelle';
+      var longUrl = 'https://github.com/williambelle/serp-preview-extension' +
+        '/blob/master/src/manifest.firefox.json';
+
+      var expectedLongUrl = 'https://github.com/williambelle/' +
+        'serp-preview-extension/blob/ ...';
+
+      var expShortUrl = serpSnippetTool.utils.buildUrl(shortUrl);
+      expShortUrl.should.equal(shortUrl);
+
+      var expLongUrl = serpSnippetTool.utils.buildTitle(longUrl);
+      expLongUrl.should.equal(expectedLongUrl);
+    });
   });
 })();
